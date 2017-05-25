8c898eb294640a1248d96237df519f723da7edca
pureainu@gmail.com <gloria@gloria-MacPro.local>
2017-05-26 01:22:02 +0900

    adding ex

--- algorithm-java/src/com/study/tryhelloworld/level2/getDayName.java
+++ algorithm-java/src/com/study/tryhelloworld/level2/getDayName.java
@@ -1,10 +1,12 @@
 package src.com.study.tryhelloworld.level2;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 2016년 1월 1일은 금요일입니다. 2016년 A월 B일은 무슨 요일일까요?
- * 두 수 A,B를 입력받아 A월 B일이 무슨 요일인지 출력하는 getDayName 함수를 완성하세요. 요일의 이름은 일요일부터 토요일까지 각각
+ * 두 수 A,B를 입력받아 A월 B일이 무슨 요일인지 출력하는 getDayName 함수를 완성하세요.
+ * 요일의 이름은 일요일부터 토요일까지 각각
 
  SUN,MON,TUE,WED,THU,FRI,SAT
 
@@ -14,12 +16,14 @@ import java.time.LocalDate;
 public class getDayName {
     public String getDayName(int a, int b)
     {
-        String answer = "";
+        String[] weekArr = new String[] {"SUN","MON","TUE","WED","THU","FRI","SAT"};
 
-        LocalDate date = LocalDate.of(2016, 1, 1);
-        System.out.println(date.getDayOfWeek().getDisplayName());
+        LocalDate startDate = LocalDate.of(2016, 1, 1);
+        LocalDate date = LocalDate.of(2016, a, b);
 
-        return answer;
+        long days = ChronoUnit.DAYS.between(startDate, date);
+        long idx = (5 + days) % 7;
+        return weekArr[(int)idx];
     }
     public static void main(String[] args)
     {
