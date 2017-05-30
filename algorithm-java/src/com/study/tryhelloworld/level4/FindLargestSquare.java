56463dae533e0b69711c202258c2f561f698ac78
gloria <pureainu@gmail.com>
2017-05-30 17:30:47 +0900

    무언가를 놓친 듯..아직 알고르즘 작성 중

--- algorithm-java/src/com/study/tryhelloworld/level4/FindLargestSquare.java
+++ algorithm-java/src/com/study/tryhelloworld/level4/FindLargestSquare.java
@@ -1,30 +1,47 @@
 package src.com.study.tryhelloworld.level4;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * O와 X로 채워진 표가 있습니다. 표 1칸은 1 x 1 의 정사각형으로 이루어져 있습니다.
  * 표에서 O로 이루어진 가장 큰 정사각형을 찾아 넓이를 반환하는 findLargestSquare 함수를 완성하세요.
  */
 public class FindLargestSquare {
+  final char chk = 'O';
+
+  public int checkSquare(char [][]board, int x, int y) {
+    String str = new String(board[y]).substring(x);
+    int cnt = board.length;
+
+    for (int i = y; i < str.indexOf('X'); i++) {
+      for (int j = x; j < cnt ; j++) {
+        if (board[i][j] != chk)  {
+          return 0;
+        }
+      }
+    }
+    return (int)Math.pow(cnt - x - str.indexOf('X'), 2);
+  }
 
   public int findLargestSquare(char [][]board)
   {
       int answer = 0;
-      final char chk = 'O';
+      int cnt = board.length;
 
-      int rowCnt = board.length;
-      int colCnt = board[0].length;
-
-      for (int i = 0; i < rowCnt; i++) {
-          for (int j = 0; j < colCnt; j++) {
-              if (board[i][j] == chk) {
-
-
-                for (int x = i; x < rowCnt; x++) {
-                  for (int y = 0; y < colCnt; y++) {
-                  }
-                }
-
+      for (int y = 0; y < cnt; y++) {
+          for (int x = 0; x < cnt; x++) {
+            if (Math.pow(cnt - y, 2) <= answer) {
+              return  answer;
+            } else if (board[y][x] == chk) {
+              int size = checkSquare(board, x, y);
+              answer = (answer < size)? size : answer;
+              if (answer > 0) {
+                break;
               }
+            }
           }
       }
 
@@ -35,11 +52,14 @@ public class FindLargestSquare {
   {
       FindLargestSquare test = new FindLargestSquare();
       char [][]board ={
-              {'X','O','O','O','X'},
-              {'X','O','O','O','O'},
-              {'X','X','O','O','O'},
-              {'X','X','O','O','O'},
-              {'X','X','X','X','X'}};
+              {'X', 'X','O','O','O','O','O','O','X'},
+              {'X', 'X','O','O','O','O','O','O','O'},
+              {'X', 'X','O','O','O','O','O','O','O'},
+              {'X', 'X','O','O','O','O','O','O','O'},
+              {'X', 'X','O','O','O','O','O','O','O'},
+              {'X', 'X','O','O','O','O','O','O','O'},
+              {'X', 'X','O','O','O','O','O','O','O'}
+      };
 
       System.out.println(test.findLargestSquare(board));
   }
