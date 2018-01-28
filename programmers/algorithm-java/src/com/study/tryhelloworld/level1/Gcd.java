e38dacd9b31447e624b54489bedb4c2060ab1c25
Gloria Jun <gloriajun@Gloriaui-MacBook-Pro.local>
2018-01-28 21:04:36 +0900

    add todo msg

--- programmers/algorithm-java/src/com/study/tryhelloworld/level1/Gcd.java
+++ programmers/algorithm-java/src/com/study/tryhelloworld/level1/Gcd.java
@@ -3,6 +3,10 @@ package src.com.study.tryhelloworld.level1;
 import java.util.Arrays;
 
 /**
+ * TODO - 코드에 버그가 있음. 확인해서 수정이 필요함.
+ */
+
+/**
  * 두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환해주는 gcdlcm 함수를 완성해 보세요.
  * 배열의 맨 앞에 최대공약수, 그 다음 최소공배수를 넣어 반환하면 됩니다.
  * 예를 들어 gcdlcm(3,12) 가 입력되면, [3, 12]를 반환해주면 됩니다.
