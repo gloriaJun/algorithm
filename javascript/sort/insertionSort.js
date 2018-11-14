c42e07ad7dd932ade3109fffa50075345fc0b5e8
gloriaJun <pureainu@gmail.com>
2018-11-14 21:33:23 +0900

    [Modify] shell sort

--- javascript/sort/insertionSort.js
+++ javascript/sort/insertionSort.js
@@ -1,5 +1,6 @@
 
 'use strict';
+let count = 0;
 
 function sort(arr) {
   const len = arr.length;
@@ -11,12 +12,15 @@ function sort(arr) {
       arr[j+1] = arr[j];
     }
     arr[j+1] = key;
-    console.log(i, j, `key: ${key}`, arr);
   }
   return arr;
 }
 
+let startTime = new Date().getTime();
 // const arr = [6, 4, 1, 7, 3, 9, 8];
-const arr = [8, 5, 6, 2, 4];
+// const arr = [8, 5, 6, 2, 4];
+const arr = [10, 8, 6, 20, 4, 3, 22, 1, 0, 15, 16];
 console.log('before', arr);
 console.log('after', sort(arr));
+let endTime = new Date().getTime();
+console.log('execution time : ', endTime - startTime);
--- javascript/sort/shellSort.js
+++ javascript/sort/shellSort.js
@@ -1,51 +1,38 @@
 
 'use strict';
+let count = 0;
+
+function insertionSort(list, first, last, gap) {
+  for (let i = first+gap; i < last; i += gap) {
+    const key = list[i];
+    let j = i-gap;
+    for (; j >= first && key < arr[j]; j -= gap) {
+      list[j+gap] = list[j];
+    }
+    list[j+gap] = key;
+  }
+  return list;
+}
 
 function sort(arr) {
   const len = arr.length;
-  let gap = len;
-  gap = parseInt(gap / 2);
-  if (gap % 2 === 0) {
-    gap++;
-  }
 
-  for (let i = 1; i < gap; i++) {
-    const key = arr[i];
-    let j = i-1;
-    for (; j >= 0 && key < arr[j]; j--) {
-      arr[j+1] = arr[j];
+  for (let gap = parseInt(len/2); gap > 0; gap = parseInt(gap/2)) {
+    if (gap % 2 === 0) {
+      gap++;
     }
-    arr[j+1] = key;
-    console.log(i, j, `key: ${key}`, arr);
+
+    for (let i = 0; i < gap; i++) {
+      insertionSort(arr, i, len, gap);
+    }
   }
-
-  // const key =
-  // for (let i = 0, j = gap; i < len && j < len; i++, j++) {
-  //   if (arr[i] > arr[j]) {
-  //     const temp = arr[i];
-  //     arr[i] = arr[j];
-  //     arr[j] = temp;
-  //   }
-  // }
-
-  // while(gap !== 1) {
-  //   // gap = parseInt(gap / 2);
-  //   // if (gap % 2 === 0) {
-  //   //   gap++;
-  //   // }
-  //
-  //   for (let i = 0, j = gap; i < len && j < len; i++, j++) {
-  //     if (arr[i] > arr[j]) {
-  //       const temp = arr[i];
-  //       arr[i] = arr[j];
-  //       arr[j] = temp;
-  //     }
-  //   }
-  // }
 
   return arr;
 }
 
+let startTime = new Date().getTime();
 const arr = [10, 8, 6, 20, 4, 3, 22, 1, 0, 15, 16];
 console.log('before', arr);
 console.log('after', sort(arr));
+let endTime = new Date().getTime();
+console.log('execution time : ', endTime - startTime);
